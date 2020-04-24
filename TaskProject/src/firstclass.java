31c9f6dc00eaacc8f51f3208f0b1103f5e54e89b
harumomo <59965650+harumomo@users.noreply.github.com>
2020-04-24 19:56:30 +0800

    Update firstclass.java

--- TaskProject/src/firstclass.java
+++ TaskProject/src/firstclass.java
@@ -2,5 +2,6 @@ public class firstclass {
     public static void main(String[] args) {
         System.out.println("out");
         System.out.println("yes");
+        System.out.println("yes");
     }
 }
